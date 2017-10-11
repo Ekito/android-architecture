29512bd0660ddb2aca0d35912ac0963b4e38c4a8
Arnaud Giuliani <agiuliani@ekito.fr>
2017-10-11 23:05:20 +0200

    udpate koin 0.4.0

---
+++ todoapp/app/src/test/java/com/example/android/architecture/blueprints/todoapp/DryRunTest.kt
@@ -0,0 +1,24 @@
+package com.example.android.architecture.blueprints.todoapp
+
+import android.app.Application
+import com.example.android.architecture.blueprints.todoapp.addedittask.AddEditTaskActivity
+import com.example.android.architecture.blueprints.todoapp.addedittask.AddEditTaskFragment
+import com.example.android.architecture.blueprints.todoapp.di.moduleList
+import com.example.android.architecture.blueprints.todoapp.taskdetail.TaskDetailActivity
+import org.junit.Test
+import org.koin.Koin
+import org.koin.android.init
+import org.mockito.Mockito
+
+/**
+ * Test Dry run / Koin Modules
+ */
+class DryRunTest {
+    @Test
+    fun todoAppModuleDryRun() {
+        Koin().init(Mockito.mock(Application::class.java))
+                // some default values
+                .properties(mapOf(TaskDetailActivity.EXTRA_TASK_ID to "", AddEditTaskFragment.ARGUMENT_EDIT_TASK_ID to "", AddEditTaskActivity.SHOULD_LOAD_DATA_FROM_REPO_KEY to false))
+                .build(moduleList()).dryRun()
+    }
+}

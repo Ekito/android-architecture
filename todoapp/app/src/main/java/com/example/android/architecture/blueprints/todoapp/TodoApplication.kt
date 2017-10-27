1db1ce73304172dc2c9384302297a28279a26596
Arnaud Giuliani <agiuliani@ekito.fr>
2017-10-27 14:54:33 +0200

    update to Koin 0.5.1

--- todoapp/app/src/main/java/com/example/android/architecture/blueprints/todoapp/TodoApplication.kt
+++ todoapp/app/src/main/java/com/example/android/architecture/blueprints/todoapp/TodoApplication.kt
@@ -2,16 +2,20 @@ package com.example.android.architecture.blueprints.todoapp
 
 import android.app.Application
 import com.example.android.architecture.blueprints.todoapp.di.moduleList
-import org.koin.android.ext.koin.startAndroidContext
+import org.koin.android.ext.android.startAndroidContext
 
 /**
  * Todo Application - main class
+ *
+ * Just run Koin context with startAndroidContext()
+ *
  */
 class TodoApplication : Application() {
 
     override fun onCreate() {
         super.onCreate()
 
+        // Start Koin
         startAndroidContext(this, moduleList())
     }
 }
